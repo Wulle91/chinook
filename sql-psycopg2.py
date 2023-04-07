3530681cad1beb3ee8bf2d4a752641bd49bb70fc
Wulle91 <>
2023-04-07 13:50:48 +0000

    hello

---
+++ sql-psycopg2.py
@@ -0,0 +1,13 @@
+import psycopg2
+connection = psycopg2.connect(database="chinook")
+
+cursor = connection.cursor()
+
+cursor.execute('SELECT * FROM "Artist"')
+
+results = sursor.fetchall()
+
+connection.close()
+
+for result in results:
+    print(result)
